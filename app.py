cedc3a06b246c474c05f0b154eb52238a6e734eb
PulserCoder <92484659+PulserCoder@users.noreply.github.com>
2023-04-17 19:11:33 +0300

    commit

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
-from flask import Flask, request, jsonify
+from typing import Dict, Optional
+from flask import Flask, request, jsonify, wrappers
+
 from utils import get_file, commands
+
 
 def create_application():
     app = Flask(__name__)
     return app
 
+
 app = create_application()
 
 
 @app.route("/perform_query", methods=["POST"])
-def perform_query():
-    arguments = request.args
-    file_name = arguments.get('file_name')
-    cmd1 = arguments.get('cmd1')
-    cmd2 = arguments.get('cmd2')
-    value1 = arguments.get('value1')
-    value2 = arguments.get('value2')
-    content = get_file(file_name)
-    res1 = commands(cmd=cmd1, value1=value1, data=content)
+def perform_query() -> wrappers.Response:
+    arguments: Dict = request.args
+    file_name: Optional[str] = arguments.get('file_name')
+    cmd1: Optional[str] = arguments.get('cmd1')
+    cmd2: Optional[str] = arguments.get('cmd2')
+    value1: Optional[str] = arguments.get('value1')
+    value2: Optional[str] = arguments.get('value2')
+    content: Optional[str] = get_file(file_name)
+    res1: str = commands(cmd=cmd1, value1=value1, data=content)
     if cmd2 is not None:
         res1 = commands(cmd=cmd2, value1=value2, data=res1)
     return jsonify(res1)
-
-
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import os
+import re
+from typing import List
 
 from config import Config
 
 
-def get_file(filename):
+def get_file(filename) -> str:
     try:
         with open(os.path.join(Config.DATA_DIR, filename), 'r', encoding='utf-8') as file:
             return file.read()
@@ -11,7 +13,7 @@ def get_file(filename):
         raise
 
 
-def commands(cmd=None, value1=None, data=None):
+def commands(cmd=None, value1=None, data=None) -> List[str]:
     if cmd == 'filter':
         return list(filter(lambda v: value1 in v, data.split('\n')))
     if cmd == 'map':
@@ -24,3 +26,7 @@ def commands(cmd=None, value1=None, data=None):
         return sorted(data.split('\n'), reverse=False)
     if cmd == 'limit':
         return [i for i in data[:int(value1)]]
+    if cmd == 'regex':
+        exp = re.compile(value1)
+        return re.findall(exp, data)
+    return []
